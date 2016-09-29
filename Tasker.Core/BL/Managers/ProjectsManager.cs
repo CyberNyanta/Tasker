1a7a064cf04a09e1cd56901e49c2cf0695c049e7
ALTEX\evgeniy.siyanko <eugenesianko@gmail.com>
2016-09-29 18:37:52 +0300

    Add entities managers

---
+++ Tasker.Core/BL/Managers/ProjectsManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Tasker.Core.DAL;
+using Tasker.Core.DL.Entities;
+
+namespace Tasker.Core.BL.Managers
+{
+    public class ProjectsManager
+    {
+        TaskRepository taskRepository;
+        ProjectsRepository projectRepository;
+
+        public ProjectsManager(string dbPatch)
+        {
+            taskRepository = new TaskRepository(dbPatch);
+            projectRepository = new ProjectsRepository(dbPatch);
+        }
+
+        public Project GetProject(int id)
+        {
+            return projectRepository.GetById(id);
+        }
+
+        public IList<Project> GetProjects()
+        {
+            return new List<Project>(projectRepository.GetAll());
+        }
+
+        public IList<Task> GetProjectTasks(int projectID)
+        {
+            return new List<Task>(taskRepository.Find(x => x.ProjectID == projectID));
+        }
+
+        public int SaveProject(Project item)
+        {
+            return projectRepository.Save(item);
+        }
+
+        public int DeleteProject(int id)
+        {
+            var projectTasks = GetProjectTasks(id).ToList();
+            for(int i =0; i< projectTasks.Count; i++)
+            {
+                taskRepository.Delete(projectTasks[i]);
+            }
+            return projectRepository.Delete(id);
+        }
+    }
+}
---
+++ Tasker.Core/BL/Managers/TaskManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Tasker.Core.DAL;
+using Tasker.Core.DL.Entities;
+
+namespace Tasker.Core.BL.Managers
+{
+    public class TaskManeger
+    {
+        TaskRepository taskRepository;
+        ProjectsRepository projectRepository;
+
+        public TaskManeger(string dbPatch)
+        {
+            taskRepository = new TaskRepository(dbPatch);
+            projectRepository = new ProjectsRepository(dbPatch);
+        }
+
+        public Task GetTask(int id)
+        {            
+            return taskRepository.GetById(id);
+        }
+
+        public IList<Task> GetTasks()
+        {
+            return new List<Task>(taskRepository.GetAll());
+        }
+
+        public IList<Task> GetProjectTasks(int projectID)
+        {
+            return new List<Task>(taskRepository.Find(x=>x.ProjectID == projectID));
+        }
+
+        public int SaveTask(Task item)
+        {
+            return taskRepository.Save(item);
+        }
+
+        public int DeleteTask(int id)
+        {
+            return taskRepository.Delete(id);
+        }
+
+
+
+    }
+}
