5c09b20bc258bbcd5959cfec830133c7f9a74923
ALTEX\evgeniy.siyanko <eugenesianko@gmail.com>
2016-10-05 18:23:10 +0300

    add fab

--- Tasker.Core/AL/ViewModels/Contracts/ITaskListViewModel.cs
+++ Tasker.Core/AL/ViewModels/Contracts/ITaskListViewModel.cs
@@ -9,5 +9,9 @@ namespace Tasker.Core.AL.ViewModels.Contracts
     public interface ITaskListViewModel: IListViewModel<Task>
     {
         bool IsSolvedTaskDisplayed { get; set; }
+
+        void ChangeStatus(Task task);
+
+        void ChangeStatus(int id);
     }
 }
--- Tasker.Droid/Activities/MainActivity.cs
+++ Tasker.Droid/Activities/MainActivity.cs
@@ -13,65 +13,52 @@ using Java.Util;
 
 using TinyIoC;
 using Com.Github.Jjobes.Slidedatetimepicker;
+using FAB = Clans.Fab.FloatingActionButton;
 
 using Tasker.Core.BL.Managers;
 using Tasker.Core.DAL.Entities;
 using Tasker.Core.AL.Utils;
-
-
-
-
-
-
+using Tasker.Core.AL.ViewModels.Contracts;
+using Android.Support.V7.App;
+using Android.Views.Animations;
 
 namespace Tasker.Droid
 {
 
 
-    [Activity (Label = "Tasker", MainLauncher = true, Icon = "@drawable/icon",Theme = "@style/Tasker")]
-	public class MainActivity : FragmentActivity
+    [Activity (Label = "Tasker", MainLauncher = true, Icon = "@drawable/icon", Theme = "@style/Tasker")]
+
+    public class MainActivity : AppCompatActivity
     {
-        protected Adapters.TaskListAdapter taskList;
-        protected IList<Task> tasks;
-        protected ListView taskListView = null;
+        private Adapters.TaskListAdapter _taskList;
+        private IList<Task> _tasks;
+        private ListView _taskListView = null;
+        private ITaskListViewModel _viewModel;
+        private FAB _fab;
+        private readonly bool hideFab;
+        private int previousVisibleItem;
 
         protected override void OnCreate (Bundle bundle)
 		{
-
+            
             base.OnCreate (bundle);
 
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.main);
 
-            taskListView = FindViewById<ListView>(Resource.Id.taskList);
-
-            var javaTime = new Java.Util.Date();
-            var cTime = DateTime.UtcNow;
-            var r = cTime.ToUnixTime();
-
-            var b = new SlideDateTimePicker.Builder(SupportFragmentManager);
-            b.SetInitialDate(javaTime);
-
-            b.SetListener(new CustomSlideDateTimeListener(this));
-            b.SetTheme(1);
-            var p = b.Build();
-            p.Show();
-
-            if (taskListView != null)
-            {
-                taskListView.ItemClick += (object sender, AdapterView.ItemClickEventArgs e) => {
-                    
-                };
-            }
-
+            _viewModel = TinyIoCContainer.Current.Resolve<ITaskListViewModel>();
+                                    
+            _taskListView = FindViewById<ListView>(Resource.Id.taskList);
+            _fab = FindViewById<FAB>(Resource.Id.fab);
+            _fab.Show(false);
+            _fab.PostDelayed(ShowFab, 300);
         }
 
 
         protected override void OnResume()
         {
             base.OnResume();
-
-            tasks = new List<Task>
+            _tasks = new List<Task>
             {
                 new Task { Title = "TaskTitle", DueDate = DateTime.Now },
                 new Task { Title = "TaskTitle1", DueDate = DateTime.Now },
@@ -81,34 +68,64 @@ namespace Tasker.Droid
                 new Task { Title = "TaskTitle5", DueDate = DateTime.Now },
                 new Task { Title = "TaskTitle6", DueDate = DateTime.Now },
                 new Task { Title = "TaskTitle7", DueDate = DateTime.Now },
+                new Task { Title = "TaskTitle", DueDate = DateTime.Now },
+                new Task { Title = "TaskTitle1", DueDate = DateTime.Now },
+                new Task { Title = "TaskTitle2", DueDate = DateTime.Now },
+                new Task { Title = "TaskTitle3", DueDate = DateTime.Now },
+                new Task { Title = "TaskTitle4", DueDate = DateTime.Now },
+                new Task { Title = "TaskTitle5", DueDate = DateTime.Now },
+                new Task { Title = "TaskTitle6", DueDate = DateTime.Now },
+                new Task { Title = "TaskTitle7", DueDate = DateTime.Now },
+                new Task { Title = "TaskTitle", DueDate = DateTime.Now },
+                new Task { Title = "TaskTitle1", DueDate = DateTime.Now },
+                new Task { Title = "TaskTitle2", DueDate = DateTime.Now },
+                new Task { Title = "TaskTitle3", DueDate = DateTime.Now },
+                new Task { Title = "TaskTitle4", DueDate = DateTime.Now },
+                new Task { Title = "TaskTitle5", DueDate = DateTime.Now },
+                new Task { Title = "TaskTitle6", DueDate = DateTime.Now },
+                new Task { Title = "TaskTitle7", DueDate = DateTime.Now }
             };
 
             // create our adapter
-            taskList = new Adapters.TaskListAdapter(this, tasks);
+            _taskList = new Adapters.TaskListAdapter(this, _tasks);
 
-            
+            _taskListView.Scroll += ListView_Scroll;
+
             //Hook up our adapter to our ListView
-            taskListView.Adapter = taskList;
+            _taskListView.Adapter = _taskList;
 
         }
 
-        public class CustomSlideDateTimeListener : SlideDateTimeListener
+        protected override void OnPause()
         {
-            MainActivity _activity;
-            public CustomSlideDateTimeListener(MainActivity activity)
+            base.OnPause();
+            _taskListView.Scroll -= ListView_Scroll;
+            ;
+        }
+
+        private void ShowFab()
+        {
+            _fab.Show(true);
+            _fab.SetShowAnimation(AnimationUtils.LoadAnimation(this, Resource.Animation.show_from_bottom));
+            _fab.SetHideAnimation(AnimationUtils.LoadAnimation(this, Resource.Animation.hide_to_bottom));
+        }
+
+        private void ListView_Scroll(object sender, AbsListView.ScrollEventArgs e)
+        {
+
+            if (e.FirstVisibleItem > previousVisibleItem)
             {
-                _activity = activity;
+                _fab.Hide(true);
             }
-            public override void OnDateTimeSet(Date p0)
+            else if (e.FirstVisibleItem < previousVisibleItem)
             {
-                Toast.MakeText(_activity, p0.ToString(), ToastLength.Long).Show();
+                _fab.Show(true);
             }
 
-            public void onDateTimeCancel()
-            {
-                Toast.MakeText(_activity,"Canceled", ToastLength.Long).Show();
-            }
+            previousVisibleItem = e.FirstVisibleItem;
         }
+
+
     }
 
     
